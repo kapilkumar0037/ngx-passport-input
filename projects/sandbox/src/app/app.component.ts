1ab676770d38cbb56d6abf70c9468e6ce31ef72f
Kapil Kumar <kapilkumar0037@gmail.com>
2024-07-13 21:20:08 +0530

    country dropdown css fix

--- projects/sandbox/src/app/app.component.ts
+++ projects/sandbox/src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
   readonly title = 'sandbox';
   countryConfig: ICountryConfig = {
     selectedCountryCode: '',
+    selectedCountryConfig: {hideCode: true}
   }
   
   passportConfig: IPassportConfig = {
